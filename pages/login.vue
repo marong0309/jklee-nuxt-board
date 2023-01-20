8d307d73cd3710dee7b28bf7d25067b9b8ff1b99
Leejunkyu <dlwnsrb0216@gmail.com>
2023-01-20 14:04:17 +0900

    login page 작업중..

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <label class="w-full h-full" for="username">ID</label>
+    <input id="username" type="text" class="w-100 border-black">
+  </form>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
