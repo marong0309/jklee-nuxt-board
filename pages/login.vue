2c38a1ea1447ec1ea0ac8ea44e295b00fb0e6d2d
Leejunkyu <dlwnsrb0216@gmail.com>
2023-01-21 21:19:48 +0900

    login page add

--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,14 @@
 <template>
-  <form>
-    <label class="w-full h-full" for="username">ID</label>
-    <input id="username" type="text" class="w-100 border-black">
-  </form>
+	<form>
+		<label class="w-full h-full" for="username">ID</label>
+		<input id="username" type="text" class="w-100 border-black">
+	</form>
 </template>
 
 <script>
 export default {
-  name: "login"
-}
+	name: 'login',
+};
 </script>
 
 <style scoped>
